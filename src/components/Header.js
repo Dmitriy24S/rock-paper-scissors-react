61d6027278880e46d2896c432aff5c06f59bc166
Dmitriy <devilins24@gmail.com>
2022-04-11 20:35:23 +0300

    init project, mobile header style

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <section className="title">
+        <span>Rock</span>
+        <span>Paper</span>
+        <span>Scissors</span>
+      </section>
+      <section className="score-container">
+        <span className="score-title">Score</span>
+        <div className="score-number">12</div>
+      </section>
+    </header>
+  );
+};
+
+export default Header;
